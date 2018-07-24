757cde3148b62eb19ed71444290f01e7ae81f6cb
3ttechs <3ttechs@gmail.com>
2018-07-24 23:27:34 +0530

    minor changes

--- MobileApp/src/pages/meeting/meeting.ts
+++ MobileApp/src/pages/meeting/meeting.ts
@@ -43,7 +43,6 @@ export class MeetingPage {
     };
 
     constructor(public navCtrl: NavController, private modalCtrl: ModalController, private alertCtrl: AlertController) {
-    //this.addEvent();
     }
 
     addEvent() {
